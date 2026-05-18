51124a13a7901b731833ec3cf44d6e92a5743ec9
damon <zhaorong@outlook.com>
2026-05-19 00:31:20 +0800

    Add mediator configuration and enable async processing in applications

--- message/src/main/java/io/theurl/message/MessageApplication.java
+++ message/src/main/java/io/theurl/message/MessageApplication.java
@@ -2,7 +2,9 @@ package io.theurl.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class MessageApplication {
 
