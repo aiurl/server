5ac2de322140de8d3059a06823d7746d3c1d6e43
damon <zhaorong@outlook.com>
2026-05-20 16:24:16 +0800

    Add SpringUtil class for application context management

---
+++ identity/src/main/java/io/theurl/identity/SpringUtil.java
@@ -0,0 +1,34 @@
+package io.theurl.identity;
+
+import lombok.Getter;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("unused")
+@Component
+public class SpringUtil implements ApplicationContextAware {
+    @Getter
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext context) throws BeansException {
+        if (applicationContext == null) {
+            applicationContext = context;
+        }
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    public static <T> T getBean(String beanName, Class<T> clazz) {
+        return applicationContext.getBean(beanName, clazz);
+    }
+
+    public static Object getBean(String beanName) {
+        return applicationContext.getBean(beanName);
+    }
+}
