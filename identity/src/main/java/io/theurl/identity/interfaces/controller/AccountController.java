a694938146218e8d07fca4095ecf8eaf4a46ad49
damon <zhaorong@outlook.com>
2026-05-24 00:52:25 +0800

    Add authentication and user management controllers with token grant DTOs

---
+++ identity/src/main/java/io/theurl/identity/interfaces/controller/AccountController.java
@@ -0,0 +1,10 @@
+package io.theurl.identity.interfaces.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+}
---
+++ identity/src/main/java/io/theurl/identity/interfaces/controller/UserController.java
@@ -0,0 +1,9 @@
+package io.theurl.identity.interfaces.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+}
