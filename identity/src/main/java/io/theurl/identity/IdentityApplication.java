031e3e027ae213f9b04b478bc837226b75d3cad9
damon <zhaorong@outlook.com>
2026-05-24 00:50:43 +0800

    Add component scans for framework and identity packages in IdentityApplication

--- identity/src/main/java/io/theurl/identity/IdentityApplication.java
+++ identity/src/main/java/io/theurl/identity/IdentityApplication.java
@@ -2,10 +2,16 @@ package io.theurl.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@ComponentScans({
+    @ComponentScan("io.theurl.framework"),
+    @ComponentScan("io.theurl.identity")
+})
 public class IdentityApplication {
 
     public static void main(String[] args) {
