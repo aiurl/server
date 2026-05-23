be2f7b326f703724433c353f2d9198e6b41909c9
damon <zhaorong@outlook.com>
2026-05-23 23:59:13 +0800

    Add ApplicationService interface and BaseApplicationService implementation for orchestrating use cases

---
+++ framework/src/main/java/io/theurl/framework/application/BaseApplicationService.java
@@ -0,0 +1,31 @@
+package io.theurl.framework.application;
+
+import com.neroyun.mediator.Mediator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.concurrent.CompletionException;
+import java.util.function.Consumer;
+
+@Service
+@RequestScope
+public class BaseApplicationService implements ApplicationService {
+    protected Mediator mediator;
+
+    protected ThreadPoolTaskExecutor mediatorTaskExecutor;
+
+    protected BaseApplicationService(ApplicationContext applicationContext) {
+        mediator = applicationContext.getBean(Mediator.class);
+        mediatorTaskExecutor = applicationContext.getBean(ThreadPoolTaskExecutor.class);
+    }
+
+    protected void handleException(Throwable throwable, Consumer<Throwable> consumer) {
+        if (throwable instanceof CompletionException exception) {
+            handleException(exception.getCause(), consumer);
+        } else {
+            consumer.accept(throwable);
+        }
+    }
+}
