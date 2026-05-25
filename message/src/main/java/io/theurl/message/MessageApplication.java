f34fdd0d99b04ecdfda082e2e9d28b1a56d2d314
damon <zhaorong@outlook.com>
2026-05-25 10:04:37 +0800

    Configure multi-tenancy in application.yaml and update Maven compiler plugin settings; enhance component scanning in MessageApplication

--- message/src/main/java/io/theurl/message/MessageApplication.java
+++ message/src/main/java/io/theurl/message/MessageApplication.java
@@ -2,10 +2,16 @@ package io.theurl.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@ComponentScans({
+    @ComponentScan("io.theurl.framework"),
+    @ComponentScan("io.theurl.message")
+})
 public class MessageApplication {
 
     public static void main(String[] args) {
