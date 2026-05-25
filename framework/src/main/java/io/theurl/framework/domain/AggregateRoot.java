2a6471ae91c0423b97f55a0eb1eab37095cdb95e
damon <zhaorong@outlook.com>
2026-05-25 22:01:47 +0800

    Enhance event handling in AggregateRoot and DomainEvent; add applyEvent method and update attach method for better event management

--- framework/src/main/java/io/theurl/framework/domain/AggregateRoot.java
+++ framework/src/main/java/io/theurl/framework/domain/AggregateRoot.java
@@ -2,6 +2,7 @@ package io.theurl.framework.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class AggregateRoot<ID extends Comparable<ID>> extends Entity<ID> implements IAggregateRoot<ID>, IHasDomainEvents {
 
@@ -29,12 +30,17 @@ public class AggregateRoot<ID extends Comparable<ID>> extends Entity<ID> impleme
 
     @Override
     public <E extends IDomainEvent> void raiseEvent(E event) {
-
+        event.attach(this);
+        this.events.add(event);
     }
 
     @Override
     public <E extends IDomainEvent> void applyEvent(E event) {
 
+    }
+
+    public <E extends IDomainEvent> void applyEvent(Consumer<E> handler, E event) {
+        handler.accept(event);
     }
 
     @Override
