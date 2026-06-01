f632f363067055d93f0e96afe6c0585944e7fc7d
damon <zhaorong@outlook.com>
2026-06-02 00:00:34 +0800

    Add search and management endpoints for bookmarks in BookmarkController

--- bundle/src/main/java/io/theurl/bundle/interfaces/controller/BookmarkController.java
+++ bundle/src/main/java/io/theurl/bundle/interfaces/controller/BookmarkController.java
@@ -3,10 +3,7 @@ package io.theurl.bundle.interfaces.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.theurl.bundle.application.contract.BundleApplicationService;
-import io.theurl.bundle.application.dto.BundleCreateDto;
-import io.theurl.bundle.application.dto.BundleItemListDto;
-import io.theurl.bundle.application.dto.BundleListDto;
-import io.theurl.bundle.application.dto.BundleUpdateDto;
+import io.theurl.bundle.application.dto.*;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +33,19 @@ public class BookmarkController {
         return service.searchAsync(criteria, from, size);
     }
 
+    @GetMapping("search")
+    @Operation(summary = "Search bookmarks", security = @SecurityRequirement(name = "bearerAuth"))
+    public CompletableFuture<List<BundleListDto>> searchAsync(@RequestParam(required = false) String keyword,
+                                                              @RequestParam(required = false, defaultValue = "0") Integer from,
+                                                              @RequestParam(required = false, defaultValue = "10") Integer size) {
+        var criteria = new HashMap<String, Object>();
+        if (keyword != null) {
+            criteria.put("keyword", keyword);
+        }
+        criteria.put("type", "bookmark");
+        return service.searchAsync(criteria, from, size);
+    }
+
     @PostMapping
     @Operation(summary = "Create a new bookmark", security = @SecurityRequirement(name = "bearerAuth"))
     public CompletableFuture<Void> createAsync(@RequestBody BundleCreateDto data, HttpServletResponse response) {
@@ -56,7 +66,7 @@ public class BookmarkController {
         return service.deleteAsync(vanity);
     }
 
-    @GetMapping("{vanity}/items/list")
+    @GetMapping("{vanity}/items")
     @Operation(summary = "Get items of a bookmark", security = @SecurityRequirement(name = "bearerAuth"))
     public CompletableFuture<List<BundleItemListDto>> searchItemsAsync(@PathVariable String vanity,
                                                                        @RequestParam(required = false) String keyword,
@@ -69,14 +79,21 @@ public class BookmarkController {
         return service.searchItemsAsync(vanity, criteria, from, size);
     }
 
-    @GetMapping("{vanity}/items/count")
-    @Operation(summary = "Get count of items in a bookmark", security = @SecurityRequirement(name = "bearerAuth"))
-    public CompletableFuture<Integer> countItemsAsync(@PathVariable String vanity,
-                                                      @RequestParam(required = false) String keyword) {
-        var criteria = new HashMap<String, Object>();
-        if (keyword != null) {
-            criteria.put("keyword", keyword);
-        }
-        return service.countItemsAsync(vanity, criteria);
+    @PostMapping("{vanity}/items")
+    @Operation(summary = "Append items to a bookmark", security = @SecurityRequirement(name = "bearerAuth"))
+    public CompletableFuture<Void> appendItemAsync(@PathVariable String vanity, @RequestBody BundleItemEditDto data) {
+        return service.appendItemAsync(vanity, data);
+    }
+
+    @PutMapping("{vanity}/items/{itemId}")
+    @Operation(summary = "Update an item in a bookmark", security = @SecurityRequirement(name = "bearerAuth"))
+    public CompletableFuture<Void> updateItemAsync(@PathVariable String vanity, @PathVariable long itemId, @RequestBody BundleItemEditDto data) {
+        return service.updateItemAsync(vanity, itemId, data);
+    }
+
+    @DeleteMapping("{vanity}/items/{itemId}")
+    @Operation(summary = "Delete an item from a bookmark", security = @SecurityRequirement(name = "bearerAuth"))
+    public CompletableFuture<Void> removeItemAsync(@PathVariable String vanity, @PathVariable long itemId) {
+        return service.removeItemAsync(vanity, itemId);
     }
 }
