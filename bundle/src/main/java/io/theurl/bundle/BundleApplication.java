4b4da47f145d229984d90606cc9058108774d543
damon <zhaorong@outlook.com>
2026-06-01 15:03:26 +0800

    Add component scanning for framework and bundle packages in BundleApplication

--- bundle/src/main/java/io/theurl/bundle/BundleApplication.java
+++ bundle/src/main/java/io/theurl/bundle/BundleApplication.java
@@ -2,10 +2,16 @@ package io.theurl.bundle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@ComponentScans({
+    @ComponentScan("io.theurl.framework"),
+    @ComponentScan("io.theurl.bundle")
+})
 public class BundleApplication {
 
     public static void main(String[] args) {
